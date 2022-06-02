4370a86c81fdbf74c1f14e1584e4456bcf6339c8
[umid shamdinli] <[umidash@code.edu.az]>
2022-06-02 20:47:04 +0400

    The reservation schedule is taken from the databases

--- liketourism/src/components/hoteldetail/Reservation.js
+++ liketourism/src/components/hoteldetail/Reservation.js
@@ -1,14 +1,30 @@
 import { Table } from "react-bootstrap";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "../../assets/sass/hoteldetail/reservation.scss";
 
 function Reservation() {
-  const region = ["Ekonomic 1 neferlik", "Ekonomic 2 neferlik", "Ekonomic 1 neferlik", "Luks 1 neferlik", "Luks 2 neferlik"];
+  // const region = ["Ekonomic 1 neferlik", "Ekonomic 2 neferlik", "Ekonomic 1 neferlik", "Luks 1 neferlik", "Luks 2 neferlik"];
 
-  const onChange = (e) => {
-    console.log(e.target.value);
-  };
+  // const onChange = (e) => {
+  //   console.log(e.target.value);
+  // };
+  const [room, setRooms] = useState([]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    loadRooms();
+  }, []);
+
+  async function loadRooms() {
+    await axios
+      .get(`https://localhost:44363/api/Reservation/GetAll`)
+      .then((res) => {
+        const result = res.data;
+        setRooms(result);
+      });
+  }
 
   return (
     <div>
@@ -22,43 +38,38 @@ function Reservation() {
                   <th>Qiymet</th>
                   <th>Seher yemeyi</th>
                   <th>Otaq sayi</th>
-
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>
-                    <select name="select" onChange={onChange}>
-                      <option>Otaq sec</option>
-                      {region.map((nameOfRegion, i) => {
-                        return <option key={i}>{nameOfRegion}</option>;
-                      })}
-                    </select>
-                  </td>
-
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <select name="select" onChange={onChange}>
-                      <option>Otaq sec</option>
-                      {region.map((nameOfRegion, i) => {
-                        return <option key={i}>{nameOfRegion}</option>;
-                      })}
-                    </select>
-                  </td>
-
-                </tr>
-                <tr>
-                  <td>
-                    Jacob
-                    <p>dddddddddddd</p>
-                  </td>
-                  <td>313 azn</td>
-                  <td>Qiymete daxildir</td>
-                  <td>2</td>
-
-                </tr>
-                
+                {room.map((rooms) => (
+                  <tr>
+                    <td>{rooms.roomType}</td>
+                    <td>{rooms.roomPrise}</td>
+                    <td>{rooms.breakfast}</td>
+                    <td>
+                      <select>
+                        <option>
+                          1
+                        </option>
+                        <option>
+                          2
+                        </option>
+                        <option>
+                          3
+                        </option>
+                        <option>
+                          4
+                        </option>
+                        <option>
+                          5
+                        </option>
+                        <option>
+                          6
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </div>
