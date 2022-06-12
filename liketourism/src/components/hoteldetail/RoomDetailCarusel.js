a537761819ffdc4965c77d738c6748df66d03266
[umid shamdinli] <[umidash@code.edu.az]>
2022-06-12 19:27:29 +0400

    roomdetailcarusel added

--- liketourism/src/components/hoteldetail/RoomDetailCarusel.js
+++ liketourism/src/components/hoteldetail/RoomDetailCarusel.js
@@ -2,44 +2,11 @@ import React from "react";
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import ImageGallery from "react-image-gallery";
 import "../../assets/sass/hoteldetail/roomdetailcarusel.scss";
-function RoomDetailCarusel() {
-  const images = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-  ];
+function RoomDetailCarusel(props) {
+ 
   return (
     <div>
-      <ImageGallery items={images} />
+      <ImageGallery items={props?.items} />
     </div>
   );
 }
--- liketourism/src/components/hoteldetail/Rooms2.js
+++ liketourism/src/components/hoteldetail/Rooms2.js
@@ -1,119 +1,206 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "../../assets/sass/hoteldetail/rooms2.scss";
-import HotelDetailImages from "./HotelDetailImages";
 import RoomDetailCarusel from "./RoomDetailCarusel";
 function Rooms2() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(300);
 
   const handless = (e) => {
     setCount(e.target.value * 300);
   };
+
+  const [room, setRooms] = useState([]);
+  const [resrooms, setResRooms] = useState([]);
+  let test = [];
+
+  if (JSON.parse(localStorage.getItem("test")) != null) {
+    test = JSON.parse(localStorage.getItem("test"));
+  }
+
+  useEffect(() => {
+    setResRooms(JSON.parse(localStorage.getItem("test")));
+    loadRooms();
+  }, []);
+
+  async function loadRooms() {
+    await axios
+      .get(`https://localhost:44363/api/Reservation/GetAll`)
+      .then((res) => {
+        const result = res.data;
+        setRooms(result);
+      });
+  }
+
+  const handle = (x) => {
+    test.push(x);
+    localStorage.setItem("test", JSON.stringify(test));
+    setResRooms(JSON.parse(localStorage.getItem("test")));
+  };
+
+  const [images, setImage] = useState([]);
+
+  useEffect(() => {
+    loadSliders();
+  }, []);
+
+  const loadSliders = async () => {
+    const result = await axios.get(
+      "https://localhost:44363/api/RoomImages/GetAll"
+    );
+    setImage(result.data);
+  };
+
+  let fullImgs = [];
+  images.forEach((image) => {
+    let data = "data:image/jpeg;base64,";
+    let fullImg = data + image.image;
+    var obj = {
+      original: fullImg,
+      thumbnail: fullImg,
+    };
+
+    fullImgs.push(obj);
+  });
+
   return (
     <div className="container">
       <div className="row mt-5">
-        <div className="col-7 roomCard">
-          <div className="col-5">
-            <img
-              className="roomImg"
-              src={require("../../assets/img/home/new.jpg")}
-              alt=""
-            />
-          </div>
-          <div className="col-4 infoss mt-3">
-            {/* <b className="head">Standart tək nəfərlik otaq</b> */}
-
-            <button
-              type="button"
-              class="btn btn-primary one"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-            >
-              2 neferlik standart otaq
-            </button>
-
-            <div
-              class="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                      Modal title
-                    </h5>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div class="modal-body">
-                    {/* <img
+        <div className="col-7">
+          {room.map((roomss) => (
+            <div className="pro">
+              <div className="col-5">
+                <img
+                  className="roomImg"
+                  src={`data:image/jpeg;base64,${roomss.image}`}
+                  alt=""
+                />
+              </div>
+              <div className="col-4 infoss mt-3">
+                <Link
+                  className="linkName"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  to={"/"}
+                >
+                  {roomss.roomType}
+                </Link>
+                <div
+                  class="modal fade"
+                  id="exampleModal"
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                          Otaq Haqqinda etrafli
+                        </h5>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body">
+                        {/* <img
                       className="roomDetailImg"
                       src={require("../../assets/img/home/new.jpg")}
                       alt=""
                     /> */}
+                        <div className="rrr d-flex">
+                          <RoomDetailCarusel
+                            autoPlay={true}
+                            showThumbnails={false}
+                            showPlayButton={false}
+                            items={fullImgs}
+                          ></RoomDetailCarusel>
 
-                    <RoomDetailCarusel></RoomDetailCarusel>
-                  </div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" class="btn btn-primary">
-                      Save changes
-                    </button>
+                          <div className="roomIconss">
+                            <h4>2 neferlik standart otaq</h4>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+                            <i class="fas fa-city"> Seher menzeresi</i>
+
+                            <p>Oda Büyüklüğü 43 m²</p>
+
+                            <p>
+                              Park yeri: ​ Yakın bir yerde (rezervasyon gerekli
+                              değildir) ücretsiz umuma açık park yeri mevcuttur.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button
+                          type="button"
+                          class="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                          Save changes
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                <h6>Sadece {roomss.remained} otaq qalib</h6>
+              </div>
+              <div className="col-3 prices">
+                <b>Otaq seç</b>
+                <select onChange={handless}>
+                  <option value={"1"}>1</option>
+                  <option value={"2"}>2</option>
+                  <option value={"3"}>3</option>
+                  <option value={"4"}>4</option>
+                </select>
+                <p>{roomss.roomPrise} AZN</p>
+
+                <button
+                  onClick={() => handle(roomss)}
+                  className="btn btn-primary twos"
+                >
+                  Rezervasiya Et
+                </button>
               </div>
             </div>
-            <h6>Sadece 13 otaq qalib</h6>
+          ))}
+        </div>
 
-            <div className="iconinfo">
-              <ul>
-                <li>
-                  <button className="btn btn-primary">Minibar</button>
-                </li>
-                <li>
-                  <button className="btn btn-primary">Parking</button>
-                </li>
-                <li>
-                  <button className="btn btn-primary">Parking</button>
-                </li>
-              </ul>
-              <ul>
-                <li>
-                  <button className="btn btn-primary">Parking</button>
-                </li>
-                <li>
-                  <button className="btn btn-primary">Parking</button>
-                </li>
-                <li>
-                  <button className="btn btn-primary">Parking</button>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col-3 prices">
-            <b>Otaq seç</b>
-            <select onChange={handless}>
-              <option value={"1"}>1</option>
-              <option value={"2"}>2</option>
-              <option value={"3"}>3</option>
-              <option value={"4"}>4</option>
-            </select>
-            <p>{count} AZN</p>
-            <button className="btn btn-primary">Rezervasiya Et</button>
-          </div>
+        <div className="col-5 sidebasket">
+          {resrooms &&
+            resrooms.map((e) => {
+              return (
+                <div className="basketss d-flex">
+                  <div className="basketrezz d-flex mt-3">
+                    <div className="reserImgs">
+                      <img
+                        className="basketImg"
+                        style={{ width: "140px" }}
+                        src={`data:image/jpeg;base64,${e.image}`}
+                      />
+                    </div>
+
+                    <p>{e.roomType}</p>
+                    <p>{e.roomPrise} AZN</p>
+                  </div>
+                </div>
+              );
+            })}
+
+          <Link to="/reservation">
+            <button className="btn btn-primary centers">Rezervasiya et</button>
+          </Link>
         </div>
       </div>
     </div>
